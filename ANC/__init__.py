9764bde2dedd2b47ffcb17cbd9fda846d14e4258
Geoff Cureton <geoff.cureton@ssec.wisc.edu>
2015-04-02 10:52:43 -0700

    Cleaned out unused imports.

--- ANC/__init__.py
+++ ANC/__init__.py
@@ -20,49 +20,7 @@ __docformat__ = 'Epytext'
 
 
 #import ProCmnPhysConst
-from Utils                     import retrieve_NCEP_grib_files
-from Utils                     import transcode_NCEP_grib_files
-from Utils                     import retrieve_METAR_files
-from Utils                     import transcode_METAR_files
-#from Utils                     import create_NCEP_grid_blobs
-#from Utils                     import create_NAAPS_grid_blobs
-#from PrecipWater               import PrecipWater
-#from SurfTemp                  import SurfTemp
-#from WindSpeed                 import WindSpeed
-#from WindDirection             import WindDirection
-#from TerrainGeopotentialHeight import TerrainGeopotentialHeight
-#from SurfPres                  import SurfPres
-#from Ozone                     import Ozone
-#from OpticalDepth              import OpticalDepth
-#from SurfGeopotentialHeight    import SurfGeopotentialHeight
-#from SpecSurfHumidity          import SpecSurfHumidity
-#from SkinTemp                  import SkinTemp
-
-#classNames = {}
-#classNames['VIIRS-ANC-Preci-Wtr-Mod-Gran'] = 'PrecipWater'
-#classNames['VIIRS-ANC-Temp-Surf2M-Mod-Gran'] = 'SurfTemp'
-#classNames['VIIRS-ANC-Wind-Speed-Mod-Gran'] = 'WindSpeed'
-#classNames['VIIRS-ANC-Wind-Direction-Mod-Gran'] = 'WindDirection'
-#classNames['VIIRS-ANC-Surf-Ht-Mod-Gran'] = 'TerrainGeopotentialHeight'
-#classNames['VIIRS-ANC-Press-Surf-Mod-Gran'] = 'SurfPres'
-#classNames['VIIRS-ANC-Tot-Col-Mod-Gran'] = 'Ozone'
-#classNames['VIIRS-ANC-Optical-Depth-Mod-Gran'] = 'OpticalDepth'
-#classNames['VIIRS-ANC-Geopot-Ht-Lev-Mod-Gran'] = 'SurfGeopotentialHeight'
-#classNames['VIIRS-ANC-Sp-Humd-Surf-Mod-Gran'] = 'SpecSurfHumidity'
-#classNames['VIIRS-ANC-Temp-Skin-Mod-Gran'] = 'SkinTemp'
-
-#from AotClimatology            import AotClimatology
-#from Bathymetry                import Bathymetry
-#from NhfOzone                  import NhfOzone
-#from NhfPresLevelTemp          import NhfPresLevelTemp
-#from NhfSpecSurfHumidity       import NhfSpecSurfHumidity
-#from NhfSurfPres               import NhfSurfPres
-#from NhfSurfTemp               import NhfSurfTemp
-#from NhfWaterVaporMixRatio     import NhfWaterVaporMixRatio
-#from NitrateDepletion          import NitrateDepletion
-#from PresLevelTemp             import PresLevelTemp
-#from SurfPresCorrection        import SurfPresCorrection
-#from TropoGeopotentialHeight   import TropoGeopotentialHeight
-#from WaterVaporMixRatio        import WaterVaporMixRatio
-
-
+from Utils import retrieve_NCEP_grib_files
+from Utils import transcode_NCEP_grib_files
+from Utils import retrieve_METAR_files
+from Utils import transcode_METAR_files
